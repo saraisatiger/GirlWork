f7911b4c9d1cfe9239f618816528cf4e556bd0dc
saraisatiger <shashem0221@gmail.com>
2019-06-04 21:37:16 -0400

    Code clean up

--- src/main/java/girlwork/controller/MainController.java
+++ src/main/java/girlwork/controller/MainController.java
@@ -1,9 +1,6 @@
 package girlwork.controller;
 
-import girlwork.repository.EnergyFarmRepository;
-import girlwork.service.AnalysisService;
 import girlwork.service.EnergyFarmService;
-import girlwork.service.TeamDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +9,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @Autowired private EnergyFarmService service;
-//    @Autowired private AnalysisService analysisService;
 
     @GetMapping("/")
     public String index() { return "index"; }
 
     @GetMapping("/data")
     public String getData(Model model) {
-//        model.addAttribute("data", service.getAllTeamData());
-//        model.addAttribute("analytics", service.getAnalysis());
         model.addAttribute("data", service.getAll());
+
+        model.addAttribute("maxTotalUnitsWind", service.maxTotalUnitsByRenewableType("WIND"));
+        model.addAttribute("maxOperatingUnitsWind", service.maxOperatingUnitsByRenewabletype("WIND"));
+        model.addAttribute("maxTotalCapacityWind", service.maxTotalCapacityByRenewableType("WIND"));
+        model.addAttribute("maxOperatingCapacityWind", service.maxOperatingCapacityByRenewableType("WIND"));
+
+        model.addAttribute("maxTotalUnitsSolar", service.maxTotalUnitsByRenewableType("SOLAR"));
+        model.addAttribute("maxOperatingUnitsSolar", service.maxOperatingUnitsByRenewabletype("SOLAR"));
+        model.addAttribute("maxTotalCapacitySolar", service.maxOperatingCapacityByRenewableType("SOLAR"));
+        model.addAttribute("maxOperatingCapacitySolar", service.maxOperatingCapacityByRenewableType("SOLAR"));
+
         return "data";
     }
 
